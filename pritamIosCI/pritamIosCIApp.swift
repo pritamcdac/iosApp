5163c079a9190da41e58fb644e2147414b31da60
Sohampa <sohampa@cybage.com>
2025-04-15 12:32:03 +0530

    Initial Commit

---
+++ pritamIosCI/pritamIosCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pritamIosCIApp.swift
+//  pritamIosCI
+//
+//  Created by administrator on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct pritamIosCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
